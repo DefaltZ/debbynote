f472ae5f51f5c3c2007ea7136dcbf46180fd9374
DefaltZ <priyongshupaul@gmail.com>
2025-03-11 11:45:37 +0530

    added more documentation(incomplete) + PD blockquote highlighting

--- src/main.js
+++ src/main.js
@@ -9,28 +9,28 @@ const themeSwitch = document.getElementById('theme-switch');
 //highlighting rebuttals
 const rebutHighlight = {
     type: 'lang',
-    regex: /!r\s+([^#\n]+)/g,
+    regex: /!r\s+([^#\n]+)/g, //type !r in markdown to highlight text in red
     replace: '<span style="background-color:rgb(253, 98, 92)">$1</span>'
 };
 
 //highlighting arguments
 const arghighlight = {
     type: 'lang',
-    regex: /!a\s+([^#\n]+)/g,
+    regex: /!a\s+([^#\n]+)/g, //type !a in markdown to highlight text in green
     replace: '<span style="background-color:rgb(152, 249, 173)">$1</span>'
 }
 
 //highlighting worldbuilding
 const worldbuildhighlight = {
     type: 'lang',
-    regex: /!wb\s+([^#\n]+)/g,
+    regex: /!wb\s+([^#\n]+)/g, //type !wb in markdown to highlight text in yellow 
     replace:  '<span style="background-color:rgb(244, 247, 114)">$1</span>'
 }
 
 //! custom extension for blockquote highlighting for British Parliamentaries
 const og1_highlight = {
     type: 'lang',
-    regex: /!og1([\s\S]+?)(?=\n\n|$)/g,
+    regex: /!og1([\s\S]+?)(?=\n\n|$)/g, //type !og1 for custom block function: Opening Government 2nd speaker 
     replace: function(match, content) {
         return `> ##OG 1st speech (PM)\n${content.split('\n').map(line => '> ' + line).join('\n')}`;
     }
@@ -38,7 +38,7 @@ const og1_highlight = {
 
 const og2_highlight = {
     type: 'lang',
-    regex: /!og2([\s\S]+?)(?=\n\n|$)/g,
+    regex: /!og2([\s\S]+?)(?=\n\n|$)/g, //type !og2 for custom block functon: Opening Government's 1st speaker
     replace: function(match, content){
         return `> ##OG 2nd speech (DPM)\n${content.split('\n').map(line => '> ' + line).join('\n')}`
     }
@@ -46,7 +46,7 @@ const og2_highlight = {
 
 const oo1_highlight = {
     type: 'lang',
-    regex: /!oo1([\s\S]+?)(?=\n\n|$)/g,
+    regex: /!oo1([\s\S]+?)(?=\n\n|$)/g, //type !oo1 for custom block function: Opening Opposition's 1st speaker
     replace: function(match, content){
         return `> ##OO 1st speech (LO)\n${content.split('\n').map(line => '> ' + line).join('\n')}`
     }
@@ -54,7 +54,7 @@ const oo1_highlight = {
 
 const oo2_highlight = {
     type: 'lang',
-    regex: /!oo2([\s\S]+?)(?=\n\n|$)/g,
+    regex: /!oo2([\s\S]+?)(?=\n\n|$)/g, //type !oo2 for custom block function: Openin Opposition's 2nd speaker
     replace: function(match, content){
         return `> ##OO 2nd speech (DLO)\n${content.split('\n').map(line => '> ' + line).join('\n')}`
     }
@@ -62,7 +62,7 @@ const oo2_highlight = {
 
 const cg1_highlight = {
     type: 'lang',
-    regex: /!cg1([\s\S]+?)(?=\n\n|$)/g,
+    regex: /!cg1([\s\S]+?)(?=\n\n|$)/g, //type !cg1 for custom block function: closing government's 1st speaker
     replace: function(match, content){
         return `> ##CG 1st speech (Gov Extension speaker)\n${content.split('\n').map(line => '> ' + line).join('\n')}`
     }
@@ -70,7 +70,7 @@ const cg1_highlight = {
 
 const cg2_highlight = {
     type: 'lang',
-    regex: /!cg2([\s\S]+?)(?=\n\n|$)/g,
+    regex: /!cg2([\s\S]+?)(?=\n\n|$)/g, //type !cg2 for custom block function: closing government 2nd speaker
     replace: function(match, content){
         return `> ##CG 2nd speech (Gov whip)\n${content.split('\n').map(line => '> ' + line).join('\n')}`
     }
@@ -93,8 +93,25 @@ const co2_highlight = {
 }
 
 //! blockquote highlighting for APDs
-
+//custom blockquote highlight for Prime Minister's speech
 const PM_highlight = {
+    type: 'lang',
+    regex: /!pm([\s\S]+?)(?=\n\n|$)/g,
+    replace: function(match, content){
+        return `> ##PM speech\n${content.split('\n').map(line => '> ' + line).join('\n')}`
+    }
+}
+
+//custom blockquote highlight for Deputy Prime Minister's speech
+const DPM_highlight = {
+    type: 'lang',
+    regex: /!dpm([\s\S]+?)(?=\n\n|$)/g,
+    replace: function(match, content){
+        return `> ##DPM speech\n${content.split('\n').map(line => '> ' + line).join('\n')}`
+    }
+}
+
+const Gwhip_highlight = {
     type: 'lang',
     regex: /!pm([\s\S]+?)(?=\n\n|$)/g,
     replace: function(match, content){
